c151acb0db7cf4698e9980a0c05f8a3b60e29938
Myst1ck <omeron1.azran@gmail.com>
2024-03-20 14:25:05 +0200

    Created the notifier and its tray icon.

---
+++ configuration.py
@@ -0,0 +1,4 @@
+from typing import Tuple
+
+
+WINDOW_SIZE: Tuple[int, int] = (300, 200)
---
+++ main.py
@@ -0,0 +1,10 @@
+from notifier import Notifier
+
+
+def main():
+    notifier: Notifier = Notifier()
+    notifier.start()
+
+
+if __name__ == '__main__':
+    main()
